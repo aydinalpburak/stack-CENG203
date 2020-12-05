f254d788012546f5064d4ad29042b1ea3c75d358
aydinalpburak <44526613+aydinalpburak@users.noreply.github.com>
2020-12-05 22:01:03 +0300

    Add files via upload

---
+++ Stack/Odev3-VeriYapiları/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Odev3_VeriYapiları
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void listBoxTemizle() {
+            listBox1.Items.Clear();
+        }        
+        liste liste = new liste();        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            liste.maxDeger = Convert.ToInt32(numericUpDown1.Value.ToString());
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            liste.maxDeger = Convert.ToInt32(numericUpDown1.Value.ToString());
+            listBoxTemizle();
+            liste.push(textBox1.Text);
+            liste.listBoxaEkle(listBox1);
+            labelStackSize.Text = liste.size().ToString();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {        
+            listBoxTemizle();
+            liste.pop();
+            textBox2.Text = liste.sonsilinen;
+            liste.listBoxaEkle(listBox1);
+            labelStackSize.Text = liste.size().ToString();
+        }
+    }
+    
+}
---
+++ Stack/Odev3-VeriYapiları/Veri.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Odev3_VeriYapiları
+{
+    public class Veri
+    {
+        
+        public string kitap_adi;
+        public Veri sonraki;
+        public Veri(string kitap_adi) {
+            this.kitap_adi = kitap_adi;
+        }
+    }
+     
+    public class liste 
+    {
+        public int maxDeger;
+        public string sonsilinen=null;
+        Veri ilk = null;
+        Veri son = null;
+        public void push(string kitap_adi) {
+            if (size()<maxDeger)
+            {
+                Veri veri = new Veri(kitap_adi);
+                if (ilk == null)
+                {
+                    ilk = veri;
+                    son = veri;
+                    son.sonraki = null;
+                }
+                else
+                {
+                    veri.sonraki = ilk;
+                    ilk = veri;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Yigin MAX Degere Ulasti!!");
+            }
+            
+        }
+        public void pop() {
+            if (size()!=0)
+            {
+                if (son == ilk)
+                {
+                    sonsilinen = ilk.kitap_adi;
+                    ilk = null;
+                    son = null;
+                }
+                else
+                {
+                    sonsilinen = ilk.kitap_adi;
+                    ilk = ilk.sonraki;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Listede Eleman Yok!!");
+            }
+        }
+        public int size() {
+            Veri gecici = ilk;
+            int sayac = 0;
+            while (gecici!=null)
+            {
+                sayac++;
+                gecici = gecici.sonraki;
+            }
+            return sayac;
+        }
+        public void listBoxaEkle(ListBox o) {
+            Veri gecici = ilk;
+            while (gecici!=null)
+            {
+                o.Items.Add(gecici.kitap_adi);
+                gecici = gecici.sonraki;
+            }
+        }
+    }
+}
